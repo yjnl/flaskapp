217e148a90647bc297a1f383b1dd26ca3a529783
Andy <acobley@computing.dundee.ac.uk>
2021-11-02 14:31:17 +0000

    removed flowerpower

--- register.py
+++ register.py
@@ -1,5 +1,4 @@
-from ApiCloud import ApiCloud
-from CSVDump import *
+
 from db import *
 from datetime import *
 import time
@@ -22,7 +21,7 @@ def hello_world():
 
 @app.route('/Sub')
 def sub_page():
-    return 'Sub Page'    
+    return 'Sub Page'
 
 @app.route('/register', methods=['GET', 'POST'])
 def login():
@@ -30,14 +29,8 @@ def login():
     if request.method == 'POST':
         username= request.form['username']
         password= request.form['password']
-# From the developer portal
-        client_id = 'aecobley@dundee.ac.uk'
-        client_secret = 'jFYfw3jQZOrAWZ675nhV4JMNapN5WCEZAG5fyN0rPA7sTz3x'
 
-        api = ApiCloud(client_id, client_secret)
-        api.login(username, password)
-        print api
-        cnx = mysql.connector.connect(user='root', password='my-secret-pw',host='mysql',)
+            cnx = mysql.connector.connect(user='root', password='my-secret-pw',host='mysql',)
         cursor = cnx.cursor()
         insert_user(cnx,cursor,username,api["access_token"])
         print api["access_token"]
@@ -54,5 +47,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-
