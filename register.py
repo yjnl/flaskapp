31809003c754d9f413e15477cb87083f8cacc7e0
Andy <acobley@computing.dundee.ac.uk>
2021-11-02 18:14:43 +0000

    updated db

--- register.py
+++ register.py
@@ -33,14 +33,10 @@ def login():
         cursor = cnx.cursor()
         insert_user(cnx,cursor,username,password)
 
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
+
+        session['logged_in'] = True
+        flash('You were logged in')
+        return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
