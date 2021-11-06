41f4c71c2dae953cf568e9d086afc8adb142f6a8
Andy <acobley@computing.dundee.ac.uk>
2021-11-06 11:22:50 +0000

    Added mongo catalogue file

--- catalogue.py
+++ catalogue.py
@@ -36,7 +36,7 @@ def sub_page():
       return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
     jResp = response.json()
     print (jResp)
-    return 'Sub Page'+jResp+response.status_code
+    return 'Sub Page'+response.status_code
 
 @app.route('/register', methods=['GET', 'POST'])
 def login():
