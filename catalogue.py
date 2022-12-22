28a6d3e182be7426d8f89d19e35e57408aeb2405
yjnl <114989228+yjnl@users.noreply.github.com>
2022-12-22 16:26:40 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -7,7 +7,7 @@ import requests
 # First we set our credentials
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+        render_template, flash
 app = Flask(__name__)
 app.debug = True
 
@@ -21,23 +21,23 @@ def video_page(video):
     response = requests.get(url)
     print (url)
     if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+        print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+        return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
     jResp = response.json()
     print (type(jResp))
     print (jResp)
     for index in jResp:
         for key in index:
-           if (key !="_id"):
-              print (index[key])
-              for key2 in index[key]:
-                  print (key2,index[key][key2])
-                  if (key2=="Name"):
-                      video=index[key][key2]
-                  if (key2=="file"):
-                      videofile=index[key][key2]
-                  if (key2=="pic"):
-                      pic=index[key][key2]
+            if (key !="_id"):
+                print (index[key])
+                for key2 in index[key]:
+                    print (key2,index[key][key2])
+                    if (key2=="Name"):
+                        video=index[key][key2]
+                    if (key2=="file"):
+                        videofile=index[key][key2]
+                    if (key2=="pic"):
+                        pic=index[key][key2]
     return render_template('video.html', name=video,file=videofile,pic=pic)
 
 @app.route('/')
@@ -52,32 +52,49 @@ def cat_page():
     print (response)
     print (response.status_code)
     if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+        print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+        return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
     jResp = response.json()
     print (type(jResp))
+    
+    
+    # Getting the categories
+    categories = []
+    caturl = "http://34.67.41.89/myflix/categories"
+    catresponse = requests.get(caturl)
+    catjResp = catresponse.json()
+    for catindex in catjResp:
+        for catkey in catindex:
+            if (catkey != "_id"):
+                categories.append(catindex[catkey].capitalize())
+    categories = sorted(categories)
+    
+    
+    
+    
+    
     html="<h2> Your Videos</h2>"
     for index in jResp:
-       #print (json.dumps(index))
-       print ("----------------")
-       for key in index:
+        #print (json.dumps(index))
+        print ("----------------")
+        for key in index:
 
-           if (key !="_id"):
-              print (index[key])
-              for key2 in index[key]:
-                  print (key2,index[key][key2])
-                  if (key2=="Name"):
-                      name=index[key][key2]
-                  if (key2=="thumb"):
-                      thumb=index[key][key2]
-                  if (key2=="uuid"):
-                      uuid=index[key][key2]  
-              html=html+'<h3>'+name+'</h3>'
-              ServerIP=request.host.split(':')[0]
-              html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-              html=html+'<img src="http://34.134.202.10/pics/'+thumb+'">'
-              html=html+"</a>"        
-              print("=======================")
+            if (key !="_id"):
+               print (index[key])
+               for key2 in index[key]:
+                    print (key2,index[key][key2])
+                    if (key2=="Name"):
+                        name=index[key][key2]
+                    if (key2=="thumb"):
+                        thumb=index[key][key2]
+                    if (key2=="uuid"):
+                        uuid=index[key][key2]  
+                html=html+'<h3>'+name+'</h3>'
+                ServerIP=request.host.split(':')[0]
+                html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
+                html=html+'<img src="http://34.134.202.10/pics/'+thumb+'">'
+                html=html+"</a>"        
+                print("=======================")
 
     return html
 
